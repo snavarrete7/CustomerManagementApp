5ce83c8d65679eed3b430fcb992700870da6b5fe
snavarrete7 <sergio.navarrete@adp.com>
2024-10-14 17:57:44 +0200

    Full crud op customerService stage3

--- CustomerService/src/main/java/com/example/customerservice/controller/CustomerController.java
+++ CustomerService/src/main/java/com/example/customerservice/controller/CustomerController.java
@@ -3,8 +3,14 @@ package com.example.customerservice.controller;
 import com.example.customerservice.domain.Customers;
 import com.example.customerservice.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +31,53 @@ public class CustomerController {
         Optional<Customers> customer = customerRepository.findById(id);
         return customer;
     }
+
+    @PostMapping
+    public ResponseEntity<?> addCustomer(@RequestBody Customers newCustomer, UriComponentsBuilder uri){
+        if(newCustomer.getName() == null || newCustomer.getEmail() == null || newCustomer.getPassword() == null){
+            return ResponseEntity.badRequest().build();
+        }
+        Customers customerSaved = customerRepository.save(newCustomer);
+        ResponseEntity<?> response = ResponseEntity.ok().build();
+        return response;
+    }
+
+
+    @GetMapping("/byname/{username}")
+    public ResponseEntity<?> lookupCustomerByNameGet(@PathVariable("username") String username,
+                                                     UriComponentsBuilder uri) {
+        Iterator<Customers> customers = customerRepository.findAll().iterator();
+        while(customers.hasNext()) {
+            Customers cust = customers.next();
+            if(cust.getName().equalsIgnoreCase(username)) {
+                ResponseEntity<?> response = ResponseEntity.ok(cust);
+                return response;
+            }
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable("id") long id){
+        if(id == 0){
+            return ResponseEntity.badRequest().build();
+        }
+        customerRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putCustomer(@PathVariable("id") long id, @RequestBody Customers newCustomer){
+        return customerRepository.findById(id)
+                .map(customer -> {
+                    customer.setName(newCustomer.getName());
+                    customer.setEmail(newCustomer.getEmail());
+                    customer.setPassword(newCustomer.getPassword());
+                    Customers updatedCustomer = customerRepository.save(customer);
+                    return ResponseEntity.ok(updatedCustomer);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
 }
